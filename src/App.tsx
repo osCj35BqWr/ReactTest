a989784f7c747fd6e03e20830565860d1bcdc6bd
osCj35BqWr <35219434+osCj35BqWr@users.noreply.github.com>
2021-05-23 16:56:06 +0900

    p43

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
 import Title from './components/Title';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="test">
       <Title />
+      <Form />
     </div>
   );
 }
