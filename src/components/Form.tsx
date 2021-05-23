43be4f91f8ed21f60746126225005628873e0970
osCj35BqWr <35219434+osCj35BqWr@users.noreply.github.com>
2021-05-23 17:38:17 +0900

    p68

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,20 @@
 // Form.tsx
+import { useState } from 'react';
+
 const Form = () => {
+    const [city, setCity] = useState<string>("");
+    const getWeather = (e: any) => {
+        e.preventDefault();
+        fetch("https://api.weatherapi.com/v1/current.json?key=064415a68df54a1b9c882942212305&q=London&aqi=no")
+        .then(res => res.json())
+        .then(data => console.log(data))
+        }
+
   return (
     <form>
-      <input type="text" name="city" placeholder="都市名" />
-      <button type="submit">Get Weather</button>
+      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+      {city}
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 };
